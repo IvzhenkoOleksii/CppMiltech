61ce51cb4f0a800257116a040b330d8d2385cb88
IvzhenkoOleksii <rty6061944@gmail.com>
2026-05-15 15:03:36 +0300

    feat: Homework6 Left only armament calculus
    Remove all code about targets, seeking and moving to targets inside main function

--- homework_06_tests/DroneCourseHomework.cpp
+++ homework_06_tests/DroneCourseHomework.cpp
@@ -18,28 +18,8 @@ int main()
     InputFile inputFile;
     DataStructs::InputData inputData = inputFile.ReadJsonFile();
 
-    // create targets
-    TargetsManager targetsManager{ inputData.ArrayTimeStep };
-
     // create drone controller
     DroneController droneController = { inputData.DroneData, inputData.SimTestStep };
-    droneController.LockTargets(targetsManager.GetTargetReferencies());
-    droneController.ChooseNearestTarget();
-
-    // create simulation controller
-    SimulationController simulation = { inputData.SimTestStep };
-    float simulationStepTime = simulation.GetSimulationStepTime();
-
-    while (simulation.IsWorking())
-    {
-        droneController.RecalculateOperationalStates(simulationStepTime);
-
-        simulation.Update();
-
-        targetsManager.UpdateTargets(simulationStepTime);
-        droneController.UpdateOperationalStates();
-        droneController.CheckIfDroneReachedFirePosition();
-    }
 
     return 0;
 }
