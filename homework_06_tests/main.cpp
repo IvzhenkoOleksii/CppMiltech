76e7c993943bd2640caf4333ee601f2d539e8614
IvzhenkoOleksii <rty6061944@gmail.com>
2026-06-03 20:27:44 +0300

    feat: Homework6 Update project, ignore .Identifier files
    Update project`s executable as "executable " not some artificial name. Rename initial .cpp file from DroneCourseHomework into executable.
    Added .Identifier and Zone.Identifier as ignored file types
    Added code to remove all already existed .Identifier files from project

--- homework_06_tests/main.cpp
+++ homework_06_tests/main.cpp
@@ -3,9 +3,14 @@
 #include "InputFile.h"
 #include "DataStructs.h"
 #include "DroneController.h"
+#include "Misc.h"
 
 auto main() -> int
 {
+  Misc msc{};
+  msc.FindAndRemoveAllZoneIdentifiers();
+  return 0;
+
   // read input file
   InputFile inputFile;
   DataStructs::InputData inputData = inputFile.ReadJsonFile("./DroneCourseHomework/DataFiles/json/Input.json");
