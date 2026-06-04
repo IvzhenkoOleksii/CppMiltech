b520a0f702681ea0ea211dd2f80af4e5a9df6c4b
IvzhenkoOleksii <rty6061944@gmail.com>
2026-06-04 19:36:29 +0300

    feat: Homework6 Return arguments pass to main.cpp
    Return arguments pass to main.cpp, argv1 is a path.
    Example:
    ./build/homework_06_tests/executable /DataFiles/json/Input.json

--- homework_06_tests/main.cpp
+++ homework_06_tests/main.cpp
@@ -1,14 +1,24 @@
-#include <string>
+#include <iostream>
 
 #include "InputFile.h"
 #include "DataStructs.h"
 #include "DroneController.h"
 
-auto main() -> int
+auto main(int argc, char** argv) -> int
 {
+  std::cout << "argc:   " << argc << "   argv[1]:    " << argv[1] << std::endl;
+
+  if (argc < 2) {
+    std::cout << "Add path to data json as argument. exiting!" << std::endl;
+    return 1;
+  }
+
   // read input file
+  std::string argument = argv[1];
+  std::string path = PROJECT_ROOT_DIR + argument;
+
   InputFile inputFile;
-  DataStructs::InputData inputData = inputFile.ReadJsonFile("./DroneCourseHomework/DataFiles/json/Input.json");
+  DataStructs::InputData inputData = inputFile.ReadJsonFile(path);
 
   // create drone controller
   DroneController droneController = {inputData.DroneData, inputData.SimTestStep};
