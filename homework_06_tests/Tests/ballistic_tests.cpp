bb5f2af03318b752c1b01aac6b0f43b56ac60f24
IvzhenkoOleksii <rty6061944@gmail.com>
2026-06-04 18:44:01 +0300

    feat: Homework6 Fixed passing tests by updating path to files
    Added inside CMAKE param: PRIVATE PROJECT_ROOT_DIR="${CMAKE_CURRENT_SOURCE_DIR}
    and using it inside ballistic_tests as path provider
    removed Misc file logic after removing all Identifier files

--- homework_06_tests/Tests/ballistic_tests.cpp
+++ homework_06_tests/Tests/ballistic_tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <string>
 
 #include "InputFile.h"
 #include "DataStructs.h"
@@ -7,35 +8,39 @@
 
 TEST(ReadFile, ReadJson)
 {
+  std::string initialString = PROJECT_ROOT_DIR;
+  std::string jsonFolderPath = initialString + "/DataFiles/json/";
+  std::string testJsonFolderPath = initialString + "/Tests/Data/";
+
   InputFile inputFile;
   DataStructs::InputData inputData;
 
   EXPECT_EXIT({ inputFile.ReadJsonFile("Input.json"); }, ::testing::ExitedWithCode(1), ".*");
-  std::cout << "[ReadJson] Ended Test 1" << '\n';
+  std::cout << "[ReadJson] Ended Test 1" << '\n' << '\n';
 
-  EXPECT_EXIT({ inputFile.ReadJsonFile("./homework_06_tests/DataFiles/json/non_existed_file.json"); }, ::testing::ExitedWithCode(1), ".*");
-  std::cout << "[ReadJson] Ended Test 2" << '\n';
+  EXPECT_EXIT({ inputFile.ReadJsonFile(jsonFolderPath.append("non_existed_file.json")); }, ::testing::ExitedWithCode(1), ".*");
+  std::cout << "[ReadJson] Ended Test 2" << '\n' << '\n';
 
-  EXPECT_NO_FATAL_FAILURE({ inputData = inputFile.ReadJsonFile("./homework_06_tests/DataFiles/json/Input.json"); });
-  std::cout << "[ReadJson] Ended Test 3" << '\n';
+  EXPECT_NO_FATAL_FAILURE({ inputData = inputFile.ReadJsonFile(jsonFolderPath.append("Input.json")); });
+  std::cout << "[ReadJson] Ended Test 3" << '\n' << '\n';
 
   EXPECT_NO_FATAL_FAILURE({ inputData.CheckData(); });
-  std::cout << "[ReadJson] Ended Test 4" << '\n';
+  std::cout << "[ReadJson] Ended Test 4" << '\n' << '\n';
 
-  EXPECT_NO_FATAL_FAILURE({ inputData = inputFile.ReadJsonFile("./homework_06_tests/Tests/Data/TestInput.json"); });
-  std::cout << "[ReadJson] Ended Test 5" << '\n';
+  EXPECT_NO_FATAL_FAILURE({ inputData = inputFile.ReadJsonFile(testJsonFolderPath.append("TestInput.json")); });
+  std::cout << "[ReadJson] Ended Test 5" << '\n' << '\n';
 
   EXPECT_EXIT({ inputData.CheckData(); }, ::testing::ExitedWithCode(1), ".*");
-  std::cout << "[ReadJson] Ended Test 6" << '\n';
+  std::cout << "[ReadJson] Ended Test 6" << '\n' << '\n';
 
   EXPECT_EXIT(
     {
-      inputData = inputFile.ReadJsonFile("./homework_06_tests/Tests/Data/WrongAmmo.json");
+      inputData = inputFile.ReadJsonFile(testJsonFolderPath.append("WrongAmmo.json"));
       ArmamentDatabase::Data armData = ArmamentDatabase::GetArmament(inputData.DroneData.AmmoType);
     },
     ::testing::ExitedWithCode(1),
     ".*");
-  std::cout << "[ReadJson] Ended Test 7" << '\n';
+  std::cout << "[ReadJson] Ended Test 7" << '\n' << '\n';
 }
 
 TEST(MathTests, Math)
