3fb027a81375d84e302b78ba4a9169015a1ca623
IvzhenkoOleksii <rty6061944@gmail.com>
2026-07-04 14:49:16 +0300

    Homework 10 One more fix for debugger to launch

--- .vscode/gdb-no-debuginfod.sh
+++ .vscode/gdb-no-debuginfod.sh
@@ -1,4 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 unset DEBUGINFOD_URLS
 export DEBUGINFOD_URLS=
-exec /usr/bin/gdb "$@"
+
+args=()
+for arg in "$@"; do
+    case "$arg" in
+        '--tty=${DbgTerm}'|'--tty=$DbgTerm')
+            tty_path="$(readlink /proc/$$/fd/2 2>/dev/null || true)"
+            if [[ -n "$tty_path" && "$tty_path" == /dev/* ]]; then
+                args+=("--tty=$tty_path")
+            fi
+            ;;
+        *)
+            args+=("$arg")
+            ;;
+    esac
+done
+
+exec /usr/bin/gdb "${args[@]}"
